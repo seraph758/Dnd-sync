c5cf15590d81a4e0d81779d34b68767fce4b2beb
seraph758 <luke.likai@gmail.com>
2026-05-10 05:58:22 +0800

    Add comment for layout ID requirement in MainActivity
    Added a comment to ensure the layout file contains a FrameLayout with ID 'settings'.

--- wear/src/main/java/de/rhaeus/dndsync/MainActivity.java
+++ wear/src/main/java/de/rhaeus/dndsync/MainActivity.java
@@ -1,7 +1,6 @@
 package de.rhaeus.dndsync;
 
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,7 +8,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // 确保你的布局文件中有这个 ID 为 settings 的 FrameLayout
         setContentView(R.layout.activity_main);
+        
         if (savedInstanceState == null) {
             getSupportFragmentManager()
                     .beginTransaction()
