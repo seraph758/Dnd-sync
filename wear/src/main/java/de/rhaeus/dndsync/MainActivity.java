159683df354a376cbf9ecffd2a216a39492f3a39
seraph758 <luke.likai@gmail.com>
2026-05-11 00:29:26 +0800

    Clean up MainActivity by removing a comment
    Removed comment about layout ID and added a newline at the end of the file.

--- wear/src/main/java/de/rhaeus/dndsync/MainActivity.java
+++ wear/src/main/java/de/rhaeus/dndsync/MainActivity.java
@@ -1,6 +1,7 @@
 package de.rhaeus.dndsync;
 
 import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -8,9 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // 确保你的布局文件中有这个 ID 为 settings 的 FrameLayout
         setContentView(R.layout.activity_main);
-        
         if (savedInstanceState == null) {
             getSupportFragmentManager()
                     .beginTransaction()
