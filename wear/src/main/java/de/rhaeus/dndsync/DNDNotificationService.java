928ba9a7b677666c14cba717a5b856de74ea6ed0
seraph758 <luke.likai@gmail.com>
2026-06-02 23:14:35 +0800

    更新 DNDNotificationService.java

--- wear/src/main/java/de/rhaeus/dndsync/DNDNotificationService.java
+++ wear/src/main/java/de/rhaeus/dndsync/DNDNotificationService.java
@@ -1,57 +1,22 @@
 package de.rhaeus.dndsync;
 
-
-import android.content.ComponentName;
-import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
 import android.service.notification.NotificationListenerService;
 import android.util.Log;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-import androidx.preference.PreferenceManager;
-
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.gms.tasks.Tasks;
-import com.google.android.gms.wearable.CapabilityClient;
-import com.google.android.gms.wearable.CapabilityInfo;
-import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.PutDataMapRequest;
+import com.google.android.gms.wearable.PutDataRequest;
 import com.google.android.gms.wearable.Wearable;
-
-import java.util.Set;
-import java.util.concurrent.ExecutionException;
 
 public class DNDNotificationService extends NotificationListenerService {
     private static final String TAG = "DNDNotificationService";
-    private static final String DND_SYNC_CAPABILITY_NAME = "dnd_sync";
-    private static final String DND_SYNC_MESSAGE_PATH = "/wear-dnd-sync";
-
     public static boolean running = false;
 
     @Override
     public void onListenerConnected() {
         Log.d(TAG, "listener connected");
         running = true;
-
-        //TODO enable/disable service based on app setting to save battery
-//        // We don't want to run a background service so disable and stop it
-//        // to avoid running this service in the background
-//        disableServiceComponent();
-//        Log.i(TAG, "Disabling service");
-//
-//        try {
-//            stopSelf();
-//        } catch(SecurityException e) {
-//            Log.e(TAG, "Failed to stop service");
-//        }
+        int currentFilter = getCurrentInterruptionFilter();
+        sendDNDSync(currentFilter);
     }
-//    private void disableServiceComponent() {
-//        PackageManager p = getPackageManager();
-//        ComponentName componentName = new ComponentName(this, DNDNotificationService.class);
-//        p.setComponentEnabledSetting(componentName,PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
-//    }
 
     @Override
     public void onListenerDisconnected() {
@@ -59,75 +24,36 @@ public class DNDNotificationService extends NotificationListenerService {
         running = false;
     }
 
-
     @Override
-    public void onInterruptionFilterChanged (int interruptionFilter) {
-        Log.d(TAG, "interruption filter changed to " + interruptionFilter);
-        // 【核心修改】：攔截由接收指令觸發的狀態變化，防止回傳給手機造成死循環
+    public void onInterruptionFilterChanged(int interruptionFilter) {
+        Log.d(TAG, "手錶本機勿擾狀態改變: " + interruptionFilter);
+
+        // 🎯 核心隔離：如果是因為收到手機請求或無障礙模擬點擊導致的變更，直接攔截，禁止回傳給手機
         if (DNDSyncListenerService.isInternalUpdate) {
-        Log.d(TAG, "檢測到手錶內部同步觸發，攔截回傳給手機的動作");
-        return;
-    }
-
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        boolean syncDnd = prefs.getBoolean("dnd_sync_key", true);
-        if(syncDnd) {
-            new Thread(new Runnable() {
-                public void run() {
-                    sendDNDSync(interruptionFilter);
-                }
-            }).start();
+            Log.d(TAG, "手錶內部更新攔截，防止反向回傳手機引發死循環");
+            return;
         }
+
+        sendDNDSync(interruptionFilter);
     }
 
+    // =====================================================
+    // 🎯 核心重構：手錶利用 DataClient 將狀態反向發往手機專線
+    // =====================================================
     private void sendDNDSync(int dndState) {
-        // https://developer.android.com/training/wearables/data/messages
+        Log.d(TAG, "手錶準備將勿擾狀態寫入 DataLayer 同步給手機: " + dndState);
 
-        // search nodes for sync
-        CapabilityInfo capabilityInfo = null;
-        try {
-            capabilityInfo = Tasks.await(
-                    Wearable.getCapabilityClient(this).getCapability(
-                            DND_SYNC_CAPABILITY_NAME, CapabilityClient.FILTER_REACHABLE));
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-            Log.e(TAG, "execution error while searching nodes", e);
-            return;
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-            Log.e(TAG, "interruption error while searching nodes", e);
-            return;
-        }
+        // 🎯 設定獨立的手錶控手機專線 Path
+        PutDataMapRequest request = PutDataMapRequest.create("/dnd_state/wear_to_phone");
+        request.getDataMap().putInt("wear_dnd_value", dndState);
+        request.getDataMap().putLong("timestamp", System.currentTimeMillis());
 
-        // send request to all reachable nodes
-        // capabilityInfo has the reachable nodes with the dnd sync capability
-        Set<Node> connectedNodes = capabilityInfo.getNodes();
-        if (connectedNodes.isEmpty()) {
-            // Unable to retrieve node with transcription capability
-            Log.d(TAG, "Unable to retrieve node with sync capability!");
-        } else {
-            for (Node node : connectedNodes) {
-                if (node.isNearby()) {
-                    byte[] data = new byte[2];
-                    data[0] = (byte) dndState;
-                    Task<Integer> sendTask =
-                            Wearable.getMessageClient(this).sendMessage(node.getId(), DND_SYNC_MESSAGE_PATH, data);
+        PutDataRequest putDataRequest = request.asPutDataRequest();
+        putDataRequest.setUrgent();
 
-                    sendTask.addOnSuccessListener(new OnSuccessListener<Integer>() {
-                        @Override
-                        public void onSuccess(Integer integer) {
-                            Log.d(TAG, "send successful! Receiver node id: " + node.getId());
-                        }
-                    });
-
-                    sendTask.addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Log.d(TAG, "send failed! Receiver node id: " + node.getId());
-                        }
-                    });
-                }
-            }
-        }
+        Wearable.getDataClient(this)
+                .putDataItem(putDataRequest)
+                .addOnSuccessListener(dataItem -> Log.d(TAG, "【手錶控手機】狀態成功寫入 DataLayer: " + dataItem.getUri()))
+                .addOnFailureListener(e -> Log.e(TAG, "【手錶控手機】狀態寫入 DataLayer 失敗", e));
     }
 }
