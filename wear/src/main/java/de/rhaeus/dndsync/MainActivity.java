ef8f37d1c8e9f6c3d20dd8f8e6510ab36d671f29
seraph758 <luke.likai@gmail.com>
2026-06-03 11:05:52 +0800

    更新 MainActivity.java

--- wear/src/main/java/de/rhaeus/dndsync/MainActivity.java
+++ wear/src/main/java/de/rhaeus/dndsync/MainActivity.java
@@ -1,6 +1,8 @@
 package de.rhaeus.dndsync;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,7 +11,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // 🎯 強制啟動 Listener Service（用於診斷）
+        Log.d("DNDSyncDebug", "MainActivity onCreate - 強制啟動 DNDSyncListenerService");
+        try {
+            startService(new Intent(this, DNDSyncListenerService.class));
+        } catch (Exception e) {
+            Log.e("DNDSyncDebug", "啟動 Listener 失敗", e);
+        }
+
         setContentView(R.layout.activity_main);
+
         if (savedInstanceState == null) {
             getSupportFragmentManager()
                     .beginTransaction()
